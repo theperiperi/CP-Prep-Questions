3390c3d25a2ebcb13d605f8d0f87c0cac03a9640
priya sridhar <121922820+theperiperi@users.noreply.github.com>
2024-02-16 13:14:54 +0530

    Update 070 Climbing Stairs.py

--- 1D DP/070 Climbing Stairs.py
+++ 1D DP/070 Climbing Stairs.py
@@ -1,9 +1,15 @@
 class Solution:
     memory={}
     def climbStairs(self, n: int) -> int:
+        #for 0 or 1 stairs only one way possible
         if n==0 or n==1:
             return 1
+        
+        #for a new computation store in memory array
         if n not in self.memory:
             self.memory[n]=self.climbStairs(n-1)+self.climbStairs(n-2)
+        
+        #this runs only once,but takes last index from memory array and returns
         return self.memory[n]
        
+   
