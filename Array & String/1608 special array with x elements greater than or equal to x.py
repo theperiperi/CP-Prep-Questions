763854bbead8a43cb4ed80af770601e23a95386d
priya sridhar <121922820+theperiperi@users.noreply.github.com>
2024-05-28 10:03:35 +0530

    Create 1608 special array with x elements greater than or equal to x.py

---
+++ Array & String/1608 special array with x elements greater than or equal to x.py
@@ -0,0 +1,13 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        nums.sort()
+        n=len(nums)
+
+        for i in range(1, n+1):
+            count=0
+            for num in nums:
+                if num>=i:
+                    count+=1
+            if count==i:
+                return count
+        return -1
