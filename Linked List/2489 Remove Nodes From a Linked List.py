ebe5eebeadc11f4d31cf1fbea2a42a2877b78b58
priya sridhar <121922820+theperiperi@users.noreply.github.com>
2024-05-30 10:48:46 +0530

    Update 2489 Remove Nodes From a Linked List.py

--- Linked List/2489 Remove Nodes From a Linked List.py
+++ Linked List/2489 Remove Nodes From a Linked List.py
@@ -18,3 +18,21 @@ class Solution:
                 prev=curr
             curr=temp
         return prev
+
+#stack
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr=head
+        stack=[]
+        while curr:
+            while stack and stack[-1].val<curr.val:
+                stack.pop()
+            stack.append(curr)
+            curr=curr.next
+        nxt=None
+        while stack:
+            curr=stack.pop()
+            curr.next=nxt
+            nxt=curr
+        return curr
+
