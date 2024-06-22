7a263cc736054ed66d5a03933e33c78792056048
Priyadharshini sridhar <121922820+theperiperi@users.noreply.github.com>
2024-06-22 17:52:13 +0530

    Create Number of Steps to Reduce a Number to Zero.py

---
+++ Python/Array & String/Number of Steps to Reduce a Number to Zero.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        counter = 0
+        while num !=0:
+            if num % 2 ==0:
+                num = num / 2
+                counter += 1
+            else:
+                num = num - 1
+                counter += 1
+        return counter
+        
