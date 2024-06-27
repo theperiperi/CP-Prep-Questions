47ca2e14ec3271112be2c9c361ba0828f5062c00
Priyadharshini sridhar <121922820+theperiperi@users.noreply.github.com>
2024-06-27 17:01:05 +0530

    Create 33 Search in Rotated Sorted Array.py

---
+++ Python/Binary Search/33 Search in Rotated Sorted Array.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        right=len(nums)-1
+        left=0
+        while left<=right:
+            mid=left+(right-left)//2
+
+            if nums[mid]==target:
+                return mid
+
+            if nums[left]<=nums[mid]:
+                if nums[left]<=target<nums[mid]:
+                    right=mid-1
+                else:
+                    left=mid+1
+            else:
+                if nums[mid]<target<=nums[right]:
+                    left=mid+1
+                else:
+                    right=mid-1
+        return -1
+
+        
