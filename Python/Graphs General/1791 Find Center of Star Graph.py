77a6a6f58e2f7aec9715d7081bbeb8a55b3e07e0
Priyadharshini sridhar <121922820+theperiperi@users.noreply.github.com>
2024-06-27 08:56:35 +0530

    Create 1791 Find Center of Star Graph.py

---
+++ Python/Graphs General/1791 Find Center of Star Graph.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findCenter(self, edges):
+        """
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        u1,v1=edges[0]
+        u2,v2=edges[1]
+
+        if u1==u2 or u1==v2:
+            return u1
+        else:
+            return v1
+        
