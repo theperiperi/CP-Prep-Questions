85876ea8516aee11ce39f4a18e5973f0187d77f8
Priyadharshini sridhar <priyasridhar101@gmail.com>
2024-08-21 23:12:09 +0530

    Create 1768 Merge String Alternatively.py

---
+++ Python/Two Pointer/1768 Merge String Alternatively.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res = ""
+        i = 0
+        while(i < len(word1) or i < len(word2)):
+            if(i<len(word1)):
+                res = res + word1[i]
+            if(i<len(word2)):
+                res = res + word2[i]
+            i+= 1
+        return res
