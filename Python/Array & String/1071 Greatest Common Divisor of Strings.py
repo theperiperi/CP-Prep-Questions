c9027ec2a57bc630d0f9540f6d87470a644746a2
Priyadharshini sridhar <priyasridhar101@gmail.com>
2024-08-21 23:08:51 +0530

    Create 1071 Greatest Common Divisor of Strings.py

---
+++ Python/Array & String/1071 Greatest Common Divisor of Strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1+str2 != str2+str1:
+            return ""
+        if len(str1)==len(str2):
+            return str1
+        if len(str1) > len(str2):
+            return self.gcdOfStrings(str1[len(str2):], str2)
+        return self.gcdOfStrings(str1, str2[len(str1):])    
