811c90cf54d86b11fef23134794d87ba374541a2
Priyadharshini sridhar <priyasridhar101@gmail.com>
2024-08-26 16:25:28 +0530

    Create 29 Divide Two Integers.py

---
+++ Python/Math/29 Divide Two Integers.py
@@ -0,0 +1,6 @@
+import math
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647  
+        return math.trunc(dividend / divisor)
