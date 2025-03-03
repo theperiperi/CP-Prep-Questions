fc251e580f0f1b51331deb37675fe4e4a94f979d
Priyadharshini sridhar <priyasridhar101@gmail.com>
2025-03-03 10:18:44 +0530

    Create 2161 Partition Array According to Given Pivot.py

---
+++ Python/Array & String/2161 Partition Array According to Given Pivot.py
@@ -0,0 +1,3 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        return [num for num in nums if num < pivot] + [num for num in nums if num == pivot] + [num for num in nums if num > pivot]
