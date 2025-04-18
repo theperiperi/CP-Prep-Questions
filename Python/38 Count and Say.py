752544dff012ae58fb2edb4ac36986a63e1f4648
Priyadharshini sridhar <priyasridhar101@gmail.com>
2025-04-18 13:32:07 +0530

    Create 38 Count and Say.py

---
+++ Python/38 Count and Say.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        res ='1'
+        for _ in range(1,n):
+            res = self.build_next(res)
+        return res
+    
+    def build_next(self,s:str)->str:
+        result =''
+        count = 1
+        for i in range(1,len(s)):
+            if s[i] == s[i-1]:
+                count+=1
+            else:
+                result+= str(count) +s[i-1]
+                count = 1
+        result+= str(count) + s[-1]
+        return result
